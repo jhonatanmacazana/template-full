bd12323c060ae463430ec66a350238a03e8a9462
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2026-03-18 11:15:44 -0500

    feat: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), nitro(), viteReact()],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
